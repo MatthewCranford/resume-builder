40c5d7ac5583e32a770671d4cfb8d6847ca3f035
Matthew Cranford <mcranfordmail@gmail.com>
2018-07-29 14:21:46 -0600

    Add bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,25 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ const model = {
+    init: function() {
+        this.bio = {
+            name: 'Matt',
+            role: 'Coder',
+            contacts: {
+                mobile: '555-555-5555',
+                email: 'place@holder.com',
+                github: 'matt@github',
+                twitter: 'matt@twitter',
+                location: 'placeholder st'
+            },
+            welcomeMessage: 'Well met!',
+            skills: ['coding', 'lifting', 'jiu-jitsu'],
+            biopic: 'placeholder.png',
+            display: function()  {
+                console.log('display');
+            }
+        }
+    }
+ }
